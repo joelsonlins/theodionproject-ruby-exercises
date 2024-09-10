e0a0ee5febc0738214ceda6bebb98be39650822f
joelsonlins <joelsonlins@hotmail.com>
2024-09-10 08:09:07 -0300

    predicate enumerables finished

--- ruby_basics/11_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
+++ ruby_basics/11_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
@@ -1,19 +1,44 @@
 def coffee_drink?(drink_list)
   # use #include? to return true when the drink_list (array) contains the string "coffee" or "espresso"
+  if drink_list.include?("coffee") || drink_list.include?("espresso")
+    true
+  else
+    false
+  end
 end
 
 def correct_guess?(guess_list, answer)
   # use #any? to return true when any element of the guess_list (array) equals the answer (number)
+  if guess_list.any?(answer)
+    true
+  else
+    false
+  end
 end
 
 def twenty_first_century_years?(year_list)
   # use #all? to return true when all of the years in the year_list (array) are between 2001 and 2100
+  if year_list.all?{|element| element >= 2001 && element <= 2100}
+    true
+  else
+    false
+  end
 end
 
 def correct_format?(word_list)
   # use #none? to return true when none of the words in the word_list (array) are in upcase
+  if word_list.none?{ |element| element == element.upcase }
+    true
+  else
+    false
+  end
 end
 
 def valid_scores?(score_list, perfect_score)
   # use #one? to return true when only one value in the score_list (hash) is equal to the perfect_score (number)
+  if score_list.one? {|k,v| v==perfect_score}
+    true
+  else
+  false
+  end
 end
